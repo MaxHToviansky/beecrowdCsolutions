2833bc96f255debd6635c42c13fc4281895ffdcb
ZeppZeppelin <mtoviansky@gmail.com>
2023-05-08 17:02:22 -0300

    Stuck on float logic for E.cpp

--- maratonaInterna2022-2/E.cpp
+++ maratonaInterna2022-2/E.cpp
@@ -5,13 +5,14 @@ int main(){
     int T;
     
     cin >> T;
-    vector<float> testes(T);
+    vector<double> testes(T);
 
     for(int i = 0; i < T; i++){
         cin >> testes[i];
         testes[i] = (25 - 3*testes[i])/2;
     }
     for(int i = 0; i<T; i++){
-        cout << trunc(testes[i]*10)/10 << endl;
+        cout.precision(1);
+        cout << fixed <<testes[i] << endl;
     }
 }
